e4d2a6c47b05060ac5329237554d352bcedb94bb
reejit <70316178+reejit@users.noreply.github.com>
2020-10-24 21:16:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,8 +29,7 @@ def start(update, context):
                              text="Hey There \n\n"
                                       "This bot can be used to encode text to Base64 language\n"
                                       "So this bot can be used as a mean for secret language\n"
-                                       "To encode text in BASE64:<code>/b64e</code> some_text\n"
-                                       "To decode text from BASE64: <code>/b64d</code> some_text\n"
+                                       "Send /commands to get a list of commands"
                                        "Made by @lolboyisback",
                              parse_mode=ParseMode.HTML)
 
